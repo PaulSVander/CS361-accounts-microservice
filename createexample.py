e915e1aeba15d62cc102cdca8c7e6c916a1cc43b
Paul Vander <paulsvander@gmail.com>
2021-11-09 18:51:03 -0800

    add example files

---
+++ createexample.py
@@ -0,0 +1,30 @@
+import socket
+import json
+
+# Create a socket object
+connectedSocket = socket.socket()
+
+# Define the port on which you want to connect
+port = 12345
+
+# connect to the server on local computer
+connectedSocket.connect(('127.0.0.1', port))
+
+x = {
+    "action": "create",
+    "username": "Bobby",
+    "description": "Bobs around"
+}
+y = json.dumps(x)
+y = y.encode('utf-8')
+ylen = len(y)
+ylen_str = str(ylen).encode('utf-8')
+ylen_buffer = b' ' * (1024 - len(ylen_str)) + ylen_str
+connectedSocket.send(ylen_buffer)
+connectedSocket.send(y)
+
+
+# receive data from the server and decoding to get the string.
+print(connectedSocket.recv(1024).decode())
+# close the connection
+connectedSocket.close()
---
+++ retrieveexample.py
@@ -0,0 +1,30 @@
+import socket
+import json
+
+# Create a socket object
+connectedSocket = socket.socket()
+
+# Define the port on which you want to connect
+port = 12345
+
+# connect to the server on local computer
+connectedSocket.connect(('127.0.0.1', port))
+
+
+a = {
+    "action": "retrieve",
+    "userToken": "0"
+}
+b = json.dumps(a)
+b = b.encode('utf-8')
+blen = len(b)
+blen_str = str(blen).encode('utf-8')
+blen_buffer = b' ' * (1024 - len(blen_str)) + blen_str
+connectedSocket.send(blen_buffer)
+connectedSocket.send(b)
+
+
+# receive data from the server and decoding to get the string.
+print(connectedSocket.recv(1024).decode())
+# close the connection
+connectedSocket.close()
